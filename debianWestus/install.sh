224a27e0ca42ef8451c6a93810b7dc4108e885bf
André <tdc@gmx.ch>
2022-04-16 18:51:46 +0200

    change user

--- debianWestus/install.sh
+++ debianWestus/install.sh
@@ -9,21 +9,21 @@ sudo echo "|_____|___|___|_| |_____|_____|   \___/|__|  |_|___|  |_____|___|_|  
 sudo apt update
 sudo apt install openvpn
 
-sudo -u mueha0 wget -P ~/ https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz
+wget -P ~/ https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.8/EasyRSA-3.0.8.tgz
 
-sudo -u mueha0 tar xvf ~/EasyRSA-3.0.8.tgz
-sudo -u mueha0 rm ~/EasyRSA-3.0.8.tgz
+tar xvf ~/EasyRSA-3.0.8.tgz
+rm ~/EasyRSA-3.0.8.tgz
 
-sudo -u mueha0 cd ~/EasyRSA-v3.0.8/
-sudo -u mueha0 cp vars.example vars
+cd ~/EasyRSA-v3.0.8/
+cp vars.example vars
 
-sudo -u mueha0 sed -i 's/#set_var EASYRSA_REQ_COUNTRY\t"US"/set_var EASYRSA_REQ_COUNTRY\t"CH"/' vars
-sudo -u mueha0 sed -i 's/#set_var EASYRSA_REQ_PROVINCE\t"California"/set_var EASYRSA_REQ_PROVINCE\t"SomePlace"/' vars
-sudo -u mueha0 sed -i 's/#set_var EASYRSA_REQ_CITY\t"San Francisco"/set_var EASYRSA_REQ_CITY\t"SomeTown"/' vars
-sudo -u mueha0 sed -i 's/#set_var EASYRSA_REQ_ORG\t"Copyleft Certificate Co"/set_var EASYRSA_REQ_ORG\t\t"Muehan Fun Company"/' vars
-sudo -u mueha0 sed -i 's/#set_var EASYRSA_REQ_EMAIL\t"me@example.net"/set_var EASYRSA_REQ_EMAIL\t"asdf@asdf.net"/' vars
-sudo -u mueha0 sed -i 's/#set_var EASYRSA_REQ_OU\t\t"My Organizational Unit"/set_var EASYRSA_REQ_OU\t\t"Muehan Org Unit"/' vars
+sed -i 's/#set_var EASYRSA_REQ_COUNTRY\t"US"/set_var EASYRSA_REQ_COUNTRY\t"CH"/' vars
+sed -i 's/#set_var EASYRSA_REQ_PROVINCE\t"California"/set_var EASYRSA_REQ_PROVINCE\t"SomePlace"/' vars
+sed -i 's/#set_var EASYRSA_REQ_CITY\t"San Francisco"/set_var EASYRSA_REQ_CITY\t"SomeTown"/' vars
+sed -i 's/#set_var EASYRSA_REQ_ORG\t"Copyleft Certificate Co"/set_var EASYRSA_REQ_ORG\t\t"Muehan Fun Company"/' vars
+sed -i 's/#set_var EASYRSA_REQ_EMAIL\t"me@example.net"/set_var EASYRSA_REQ_EMAIL\t"asdf@asdf.net"/' vars
+sed -i 's/#set_var EASYRSA_REQ_OU\t\t"My Organizational Unit"/set_var EASYRSA_REQ_OU\t\t"Muehan Org Unit"/' vars
 
-sudo -u mueha0 export EASYRSA_BATCH=1
-sudo -u mueha0 ./easyrsa init-pki
-sudo -u mueha0 ./easyrsa build-ca nopass
+export EASYRSA_BATCH=1
+./easyrsa init-pki
+./easyrsa build-ca nopass
